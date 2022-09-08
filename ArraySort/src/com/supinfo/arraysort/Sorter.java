a374f8dc02a79d5cb3fad671aed48d6e97925414
Raccoon-In-A-Jar <108840662+Raccoon-In-A-Jar@users.noreply.github.com>
2022-09-09 00:30:19 +0200

    Add files via upload

---
+++ ArraySort/src/com/supinfo/arraysort/Sorter.java
@@ -0,0 +1,41 @@
+package com.supinfo.arraysort;
+
+public class Sorter {
+
+	
+	int[] mix = {5, 1, 4, 3, 2};
+	
+	//Sorts the array with the bubble sort algorithm
+	static int[] bubbleSort(int[] array) {
+		
+		
+		return array;
+	}
+	
+	static int[] selectionSort(int[] array)
+    {
+		return array;
+    }
+
+    static int[] insertionSort(int[] array)
+    {
+    	return array;
+    }
+
+    static void displayArray(int[] array)
+    {
+    	
+    }
+    
+	
+    
+    
+    public static void main(String[] args) {
+		
+	
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
